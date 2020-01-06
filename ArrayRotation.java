0886ded9910eb7e28631cd9a74fcbe7eb63de4ac
Charity Kathure <kathurecharity505@gmail.com>
2020-01-06 15:36:27 +0300

    Update ArrayRotation.java

--- ArrayRotation.java
+++ ArrayRotation.java
@@ -31,7 +31,7 @@ public class ArrayRotation {
 	}
 	public static void main(String [] args) {
 		int[]arr = {1,2,3,4,5,6,7};
-		rotate(arr, 2 , 7);
+		rotate(arr, 2 , arr.length);
 		
 		printArray(arr);
 	}
