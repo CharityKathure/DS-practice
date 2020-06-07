79c26b36e40a8bda8a4f01a160920316dbd47244
Charity Kathure <kathurecharity505@gmail.com>
2020-06-07 23:47:15 +0300

    Add files via upload

---
+++ BinaryTreeeBasics.java
@@ -0,0 +1,114 @@
+package binaryTrees;
+
+class Node{
+	int key;
+	Node left, right;
+	
+	Node(int item){
+		key = item;
+		left = right = null;
+	}
+}
+
+
+public class BinaryTreeeBasics {
+	Node root;
+	
+	void printInorder(Node node) {
+		if(node == null) {
+			return; 
+		} else {
+			printInorder(node.left);
+			System.out.print(node.key + " ");
+			printInorder(node.right);
+		}
+	}
+	 
+	void printPostOrder(Node node) {
+		if(node == null) {
+			return; 
+		} else { 
+			printPostOrder(node.left);
+			printPostOrder(node.right);
+			System.out.print(node.key + " ");
+		}
+	}
+	
+	void printPreOrder(Node node) {
+		if(node == null) {
+			return;
+		} else {
+			System.out.print(node.key + " ");
+			printPreOrder(node.left);
+			printPreOrder(node.right);
+		}
+	}
+	
+	int treeHeight(Node node) {
+		//check if tree is empty and return 0 if empty
+		if(node == null) {
+			return 0;
+		}
+		
+		int leftHeight = treeHeight(node.left);
+		int rightHeight = treeHeight(node.right);
+		
+		//return the bigger
+		if(leftHeight > rightHeight) {
+			return leftHeight + 1;
+		} else {
+			return rightHeight + 1;
+		}
+	}
+	
+	void printLevelOrder(Node node) {
+		int h = treeHeight(node);
+		for(int i=1; i<=h; i++) {
+			printGivenLevel(node, i);
+		}
+	}
+	
+	void printGivenLevel(Node node, int level) {
+		if(node == null) {
+			return;
+		}
+		if(level == 1) {
+			System.out.print(node.key + " ");
+		}
+		if(level > 1) {
+			printGivenLevel(node.left, level-1);
+			printGivenLevel(node.right, level-1);
+		}
+	}
+	 
+	public static void main(String args[]) {
+		
+		BinaryTreeeBasics bt = new BinaryTreeeBasics();
+		
+		bt.root = new Node(1);
+		
+		bt.root.left = new Node(2);
+		bt.root.right = new Node(3);
+		
+		bt.root.left.left = new Node(4);
+		bt.root.left.right = new Node(5);
+		
+		System.out.print("InOrder Traversal: ");
+		bt.printInorder(bt.root);
+		System.out.println();
+		
+		System.out.print("PreOrder Traversal: ");
+		bt.printPreOrder(bt.root);
+		System.out.println();
+		
+		System.out.print("PostOrder Traversal: ");
+		bt.printPostOrder(bt.root);
+		System.out.println();
+		
+		System.out.print("Level Order/Breadth First Traversal: ");
+		bt.printLevelOrder(bt.root);
+		System.out.println();
+	}
+	
+
+}
