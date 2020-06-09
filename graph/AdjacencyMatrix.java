3f9c52e8a71d3c8307362b10dd9f0f35e1a30e0d
Charity Kathure <kathurecharity505@gmail.com>
2020-06-09 21:53:49 +0300

    Update AdjacencyMatrix.java

--- graph/AdjacencyMatrix.java
+++ graph/AdjacencyMatrix.java
@@ -5,18 +5,18 @@ import java.util.ArrayList;
 /*Vector implementation*/
 public class AdjacencyMatrix {
 	
-	static void addEdge(ArrayList<ArrayList<Integer>> obj, int a, int b) {
-		obj.get(a).add(b);
-		obj.get(b).add(a);
+	static void addEdge(ArrayList<ArrayList<Integer>> adj, int a, int b) {
+		adj.get(a).add(b);
+		adj.get(b).add(a);
 	}
 	
-	static void printGraph(ArrayList<ArrayList<Integer>> obj) {
-		int v = obj.size();
+	static void printGraph(ArrayList<ArrayList<Integer>> adj) {
+		int v = adj.size();
 		for(int i=0; i<v; i++) {
 			System.out.print("vertex " +i);
-			ArrayList<Integer> ob = obj.get(i);
-			for(int j=0; j<ob.size(); j++) {
-				System.out.print(" -> " + ob.get(j));
+			ArrayList<Integer> ad = adj.get(i);
+			for(int j=0; j<ad.size(); j++) {
+				System.out.print(" -> " + ad.get(j));
 			}
 			System.out.println();
 		}
@@ -25,20 +25,20 @@ public class AdjacencyMatrix {
 	public static void main(String args[]) {
 		int V = 5;
 		
-		ArrayList<ArrayList<Integer>> obj = new ArrayList<>();
+		ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 		for(int i=0; i<V; i++) {
-			obj.add(new ArrayList<Integer>());
+			adj.add(new ArrayList<Integer>());
 		}
 		
-		addEdge(obj, 0, 1);
-		addEdge(obj, 0, 4);
-		addEdge(obj, 1, 2);
-		addEdge(obj, 1, 3);
-		addEdge(obj, 1, 4);
-		addEdge(obj, 2, 3);
-		addEdge(obj, 3, 4);
+		addEdge(adj, 0, 1);
+		addEdge(adj, 0, 4);
+		addEdge(adj, 1, 2);
+		addEdge(adj, 1, 3);
+		addEdge(adj, 1, 4);
+		addEdge(adj, 2, 3);
+		addEdge(adj, 3, 4);
 		
-		printGraph(obj);
+		printGraph(adj);
 		
 	}
 }
