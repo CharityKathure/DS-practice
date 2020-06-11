cd6f655f7fb1315a22085e44d81735af2b7f7647
Charity Kathure <kathurecharity505@gmail.com>
2020-06-11 11:47:51 +0300

    Create MotherVertex.java

---
+++ graph/MotherVertex.java
@@ -0,0 +1,86 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+//to be optimized. current time complexity is O(V*V)
+	
+public class MotherVertex {
+	
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	
+	static void addEdge(int s, int e){
+		graph.get(s).add(e);
+	}
+	
+	static void DFS(int s){
+		boolean visited[] = new boolean[graph.size()];
+		DFSUtil(visited, s);
+	}
+	
+	static void DFSUtil(boolean visited[], int s) {
+		visited[s] = true;
+		
+		Queue<Integer> allVerticesofS = BFS(s);
+		Iterator<Integer> i = graph.get(s).listIterator();
+		while(i.hasNext()) {
+			int n = i.next();
+			if(!visited[n]) {
+				DFSUtil(visited, n);
+			}
+		}
+		
+		if(allVerticesofS.size() == graph.size()) {
+			System.out.println("\t"+s);
+		}
+		
+	}
+	
+	static Queue<Integer> BFS(int s){
+		boolean visited[] = new boolean[graph.size()];
+		
+		Queue<Integer> q = new LinkedList<>();
+		Queue<Integer> allVertices = new LinkedList<>();
+		visited[s] = true;
+		q.add(s);
+		
+		while(!q.isEmpty()) {
+			s = q.peek();
+			q.poll();
+			
+			
+			allVertices.add(s);
+			Iterator<Integer> i = graph.get(s).listIterator();
+			while(i.hasNext()) {
+				int n = i.next();
+				if(!visited[n]) {
+					visited[n] = true;
+					q.add(n);
+				}
+			}
+		}
+		return allVertices;
+	}
+	
+	
+	
+	public static void main(String args[]) {
+		int V = 5;
+		for(int i=0; i<V; i++) {
+			ArrayList<Integer> a = new ArrayList<>();
+			graph.add(a);
+		}
+		
+		addEdge(1, 0);
+		addEdge(2, 1);
+		addEdge(0, 3);
+		addEdge(0, 2);
+		addEdge(3, 4);
+		
+		System.out.println("Mother Vertices(Using BruteForce Method): ");
+		DFS(1);
+	}
+
+}
