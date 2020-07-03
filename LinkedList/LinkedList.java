450dbc92167a50f0e680d1436c196b6c79a96555
Charity Kathure <kathurecharity505@gmail.com>
2020-07-03 23:04:33 +0300

    Update LinkedList.java

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,19 +1,7 @@
 package linkedLists;
 
-class Node {
-	int data;
-	Node next;
-	
-	Node(int d){
-		data = d;
-		next = null;
-	}
-}
-
 public class LinkedList {
 	static Node head;
-	
-	
 	
 	static void printList() {
 		Node n = head;
@@ -21,6 +9,79 @@ public class LinkedList {
 			System.out.print(n.data + " ");
 			n = n.next;
 		}
+	}
+	static void add_push(int data){
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+		
+		return;
+	}
+	
+	static void insertAfter(Node n, int d) {
+		//base case
+		if(n == null) {
+			return;
+		}
+		
+		Node newNode = new Node(d);
+		
+		newNode.next = n.next;
+		n.next = newNode;
+		
+		return;
+	}
+	
+	static void insertAtTheEnd(int d) {
+		//Initialize new node
+		Node newNode = new Node(d);
+		
+		//base case
+		if(head==null) {
+			head = newNode;
+			
+			return;
+		}		
+		
+		
+		//since it will be the last node make next node null
+		newNode.next = null;
+		
+		//iterate to the last node
+		Node last = head;
+		while(last.next != null) {
+			last = last.next;
+		}
+		
+		//make the lastnode.next to be the new created node
+		last.next = newNode;
+	}
+	
+	static void deleteNode(int d) {
+		Node temp = head, prev = null;
+		
+		//if node to be deleted is head
+		if(temp != null && temp.data == d) {
+			head = temp.next;
+			return;
+		}
+		
+		//if node is not head
+		while(temp != null && temp.data != d) {
+			prev = temp;
+			temp = temp.next;
+		}
+		
+		//if node not found in linked list
+		if(temp == null) {
+			return;
+		}
+		
+		//detach
+		prev.next = temp.next;
+		
+		return;
+		
 	}
 	
 	public static void main(String args[]) {
@@ -34,6 +95,23 @@ public class LinkedList {
 		second.next = third;
 		
 		//traversal
+		printList();
+		
+		System.out.print("\n");
+		add_push(4);
+		
+		printList();
+		
+		insertAfter(second, 5);
+		System.out.print("\n");
+		printList();
+		
+		insertAtTheEnd(6);
+		System.out.print("\n");
+		printList();
+		
+		deleteNode(1);
+		System.out.print("\n");
 		printList();
 	}
 	
