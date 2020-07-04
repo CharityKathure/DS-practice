188785d858a7c8890f549c60f8b216856686d451
Charity Kathure <kathurecharity505@gmail.com>
2020-07-05 02:43:37 +0300

    Update Anagram.java

--- String Manipulation/Anagram.java
+++ String Manipulation/Anagram.java
@@ -5,7 +5,7 @@ package stringManipulation;
  * pseudocode:
  * 1. if length of string 1 is different from length of string 2 the two strings are not anagrams
  * 2. else if lengths are the same:
- * 3. 		check if each characters of string 1is in string 2
+ * 3. 		check if each characters of string 1 is in string 2
  * 4. 		if any character in one of the strings is not in the other, then the two strings are not anagrams
  * **/
 
