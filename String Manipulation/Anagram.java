192141ff0b89d35863cd2cb8ea7809d7d57f5707
Charity Kathure <kathurecharity505@gmail.com>
2020-07-05 02:50:41 +0300

    Update Anagram.java

--- String Manipulation/Anagram.java
+++ String Manipulation/Anagram.java
@@ -3,14 +3,18 @@ package stringManipulation;
 /**write a program that checks if two strings are anagrams of each other
  * 
  * pseudocode:
- * 1. if length of string 1 is different from length of string 2 the two strings are not anagrams
- * 2. else if lengths are the same:
- * 3. 		check if each characters of string 1 is in string 2
- * 4. 		if any character in one of the strings is not in the other, then the two strings are not anagrams
+ * 1. Remove spaces from both strings
+ * 2. if length of string 1 is different from length of string 2 the two strings are not anagrams
+ * 3. else if lengths are the same:
+ * 4. 		check if each characters of string 1is in string 2
+ * 5. 		if any character in one of the strings is not in the other, then the two strings are not anagrams
  * **/
 
 public class Anagram {
 	static boolean anagramCheck(String s1, String s2) {
+		s1 = s1.replaceAll("\\s+","");
+		s2 = s2.replaceAll("\\s+","");
+		
 		if(s1.length() != s2.length()) {
 			return false;
 		} else {
@@ -26,8 +30,8 @@ public class Anagram {
 	}
 		
 	public static void main(String args[]) {
-		String s1 = "fried";
-		String s2 = "fired";
+		String s1 = "Dormitory";
+		String s2 = "Dirtyroom";
 		
 		System.out.print(anagramCheck(s1, s2));
 	}
