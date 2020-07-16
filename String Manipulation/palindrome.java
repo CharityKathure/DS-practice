0e7e17ae0c0c8648fd02567b1f0a39c334afc3de
Charity Kathure <kathurecharity505@gmail.com>
2020-07-16 21:42:13 +0300

    Update palindrome.java

--- String Manipulation/palindrome.java
+++ String Manipulation/palindrome.java
@@ -31,6 +31,45 @@ public class Palindrome {
 		}
 	}
 	
+	static boolean isPalindrome(String s) {
+		s = s.replaceAll(" ", "");
+		s = s.replaceAll("[^a-zA-Z0-9]", " ").toUpperCase();
+		
+		///check length of string
+		///reverse the first half of the substring and check if it is equal to the other half of the string
+		int stringLength = s.length();
+		int mid = stringLength/2;
+		
+		String s1 = s.substring(0, mid);
+		String s2 = "";
+		if(stringLength%2 == 0) {
+			s2 = s.substring(mid, stringLength);
+		} else {
+			s2 = s.substring(mid + 1, stringLength);
+		}
+		s1 = reverseString(s1);
+		
+		System.out.println(mid + ": " + s + " - "  + s1 + " - " + s2);
+		if(s1.equals(s2)) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	static String reverseString(String s){
+		char[] stringChars = s.toCharArray();
+		int leftPtr = 0;
+		int rightPtr = s.length() - 1;
+		for(leftPtr = 0; leftPtr< rightPtr; leftPtr++, rightPtr--) {
+			char temp = stringChars[leftPtr];
+			stringChars[leftPtr] = stringChars[rightPtr];
+			stringChars[rightPtr] = temp;
+		}
+		
+		return new String(stringChars);
+	}
+	
 	static String longestPalindrome(String s) {
 		/*Given a string, find the longest substring which is palindrome.*/
 		String longestPalindrome = "";
@@ -46,9 +85,13 @@ public class Palindrome {
 		return longestPalindrome;
 	}
 	
+	
 	public static void main(String args[]) {
 		String s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
 		System.out.println(longestPalindrome(s));
+		
+		
+		System.out.println(isPalindrome("Live on time, emit no evil"));
 	}
 	
 
